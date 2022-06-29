05b0bc9142967198522cfcf8e46a56a12ec54815
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-06-29 16:14:58 -0300

    Exercicio 3 - finalizado.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
+function getEmployeeByName(emplName) {
+  if (emplName === undefined) {
+    return {};
+  }
+  const { employees } = data;
+  return employees.find((empl) => empl.firstName === emplName || empl.lastName === emplName);
 }
 
 module.exports = getEmployeeByName;
