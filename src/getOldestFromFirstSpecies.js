005105f7489bc291b8ec9f292cf5a58f2f8bbffd
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-06-30 23:46:18 -0300

    Requisito 10 - finalizado - absorvendo HoFsgit add .

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+const { species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const idSpecieToFindOld = employees.filter((employee) => employee.id === id)[0].responsibleFor[0];
+  const oldestAnimal = species.filter((specie) => specie.id === idSpecieToFindOld)[0].residents
+    .sort((a, b) => b.age - a.age)[0];
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
