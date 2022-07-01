a79cf95967f395c031c67584eb9f293f8776e688
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-06-30 23:02:34 -0300

    Requisito 9 - finalizado.

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,50 @@
 const data = require('../data/zoo_data');
 
+const { hours } = data;
+const { species } = data;
+
+const animals = species.map((specie) => specie.name);
+const days = Object.keys(hours);
+
+function getAllSchedule() {
+  const scheduleObj = {};
+  Object.keys(hours).forEach((day) => {
+    if (day !== 'Monday') {
+      scheduleObj[day] = {
+        officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+        exhibition: species
+          .filter((specie) => specie.availability.includes(day))
+          .map((specie) => specie.name),
+      };
+    } else {
+      scheduleObj[day] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    }
+  });
+  return scheduleObj;
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (scheduleTarget === undefined) {
+    return (getAllSchedule());
+  }
+  if (animals.includes(scheduleTarget)) {
+    return species.filter((specie) => specie.name === scheduleTarget)[0].availability;
+  }
+  if (days.includes(scheduleTarget)) {
+    return { [scheduleTarget]: getAllSchedule()[scheduleTarget] };
+  }
+  return (getAllSchedule());
 }
 
 module.exports = getSchedule;
+
+// 'Tuesday': {
+//   'officeHour': 'Open from 8am until 6pm',
+//   'exhibition': [ 'lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes' ],
+// },
+
+// hours: {
+//   Tuesday: { open: 8, close: 6 },
