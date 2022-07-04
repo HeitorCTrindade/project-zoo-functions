7346f70c780fc58f7ec8daa6c0dac267a1092432
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-07-04 18:49:59 -0300

    Requisito 14 refatorado e otimizado

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,65 +2,54 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-function getFullAnimalMap() {
+const filterByLocation = (speciesLocation) => (species
+  .filter((specie) => (specie.location === speciesLocation))
+  .map(((neSpeciesLocation) => neSpeciesLocation.name)));
+
+function getFullAnimalMap() { // cria o objeto de nome de especies de animais por localização
   const mapZooObj = {
-    NE: species.filter((specie) => (specie.location === 'NE'))
-      .map(((neSpeciesLocation) => neSpeciesLocation.name)),
-    NW: species.filter((specie) => (specie.location === 'NW'))
-      .map(((nwSpeciesLocation) => nwSpeciesLocation.name)),
-    SE: species.filter((specie) => (specie.location === 'SE'))
-      .map(((seSpeciesLocation) => seSpeciesLocation.name)),
-    SW: species.filter((specie) => (specie.location === 'SW'))
-      .map(((swSpeciesLocation) => swSpeciesLocation.name)),
+    NE: filterByLocation('NE'),
+    NW: filterByLocation('NW'),
+    SE: filterByLocation('SE'),
+    SW: filterByLocation('SW'),
   };
   return mapZooObj;
 }
 
-const filterByLocationAndSex = (speciesLocation, sexAnimal) => (species
-  .filter((specie) => (specie.location === speciesLocation))
-  .map(((neSpeciesLocation) => ({ [neSpeciesLocation.name]: neSpeciesLocation.residents
-    .filter((resident) => (resident.sex === sexAnimal) || (sexAnimal === undefined))
-    .map((resident) => resident.name) }))));
+const filterByLocationAndSex = (speciesLocation, sexAnimal, mustSort) => {
+  if (mustSort === true) {
+    return species
+      .filter((specie) => (specie.location === speciesLocation))
+      .map(((neSpeciesLocation) => ({ [neSpeciesLocation.name]: neSpeciesLocation.residents
+        .filter((resident) => (resident.sex === sexAnimal) || (sexAnimal === undefined))
+        .map((resident) => resident.name).sort() })));
+  }
+  return species
+    .filter((specie) => (specie.location === speciesLocation))
+    .map(((neSpeciesLocation) => ({ [neSpeciesLocation.name]: neSpeciesLocation.residents
+      .filter((resident) => (resident.sex === sexAnimal) || (sexAnimal === undefined))
+      .map((resident) => resident.name) })));
+};
 
-function getFullAnimalMapWithNames(sexAnimal) {
+function getFullAnimalMapWithNames(sexAnimal, mustSort) { // cria o objeto de nome dos animais de cada especies por localização
   const mapZooObjWithAnimalsNames = {
-    NE: filterByLocationAndSex('NE', sexAnimal),
-    NW: filterByLocationAndSex('NW', sexAnimal),
-    SE: filterByLocationAndSex('SE', sexAnimal),
-    SW: filterByLocationAndSex('SW', sexAnimal),
+    NE: filterByLocationAndSex('NE', sexAnimal, mustSort),
+    NW: filterByLocationAndSex('NW', sexAnimal, mustSort),
+    SE: filterByLocationAndSex('SE', sexAnimal, mustSort),
+    SW: filterByLocationAndSex('SW', sexAnimal, mustSort),
   };
   return mapZooObjWithAnimalsNames;
 }
 
-const filterByLocationAndSexSort = (speciesLocation, sexAnimal) => (species
-  .filter((specie) => (specie.location === speciesLocation))
-  .map(((neSpeciesLocation) => ({ [neSpeciesLocation.name]: neSpeciesLocation.residents
-    .filter((resident) => (resident.sex === sexAnimal) || (sexAnimal === undefined))
-    .map((resident) => resident.name).sort() }))));
-
-function getFullAnimalMapWithNamesSort(sexAnimal) {
-  const mapZooObjWithAnimalsNamesSort = {
-    NE: filterByLocationAndSexSort('NE', sexAnimal),
-    NW: filterByLocationAndSexSort('NW', sexAnimal),
-    SE: filterByLocationAndSexSort('SE', sexAnimal),
-    SW: filterByLocationAndSexSort('SW', sexAnimal),
-  };
-  return mapZooObjWithAnimalsNamesSort;
-}
-
 function getAnimalMap(options) {
-  if (options === undefined) {
+  if (options === undefined) { // defult, sem nada retorna o mapa das especies de animais por localização
     return getFullAnimalMap();
   }
   const { includeNames, sex, sorted } = options;
-
-  if (includeNames === true && sorted === true) {
-    return getFullAnimalMapWithNamesSort(sex);
+  if (includeNames === true) { // se pede com os nomes dos animais,retorna o mapa dos nomes de animais de cada especies por localização
+    return getFullAnimalMapWithNames(sex, sorted); // filtro por sexo e/ou ordenação dos nomes, para o problema, so ocorre em caso de listagem por nome de cada espécime
   }
-  if (includeNames === true) {
-    return getFullAnimalMapWithNames(sex);
-  }
-  return getFullAnimalMap();
+  return getFullAnimalMap(); // feito dos filtros: sem parametros, com nomes e/ou sex e/ou ordenados, so restam possíbilidades em que deve-se retornar o mapa completo
 }
 
 module.exports = getAnimalMap;
